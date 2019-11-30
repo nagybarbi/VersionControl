b0fd5ef22bf42c8fd1abf84df0a30301aeb32e43
Barbara Nagy <barbara.nagy4@stud.uni-corvinus.hu>
2019-11-30 19:36:19 +0100

    save to file

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -8,16 +8,12 @@ namespace UserMaintenance.Entities
 {
     public class User
     {
+        public override string ToString()
+        {
+            return ID + " " + FullName;
+        }
+        
         public Guid ID { get; set; } = Guid.NewGuid();
         public string FullName { get; set; }
-
-        // Ugyanaz a FullName property kompaktabb formában is írható
-        /*
-        public string FullName
-            => string.Format(
-                "{0} {1}",
-                LastName,
-                FirstName);
-        */
     }
 }
