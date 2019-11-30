4f25d116d09e3eb20be88a1753aca215906fbf7e
Barbara Nagy <barbara.nagy4@stud.uni-corvinus.hu>
2019-11-30 18:53:00 +0100

    first/last name -> fullname

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -18,8 +18,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource.LastName; // label1
-            label2.Text = Resource.FirstName; // label2
+            label1.Text = Resource.FullName; // label1
             button1.Text = Resource.Add; // button1
 
             listBox1.DataSource = users;
@@ -31,8 +30,7 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
             };
             users.Add(u);
         }
